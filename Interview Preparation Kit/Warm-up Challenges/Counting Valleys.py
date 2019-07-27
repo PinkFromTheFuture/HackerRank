f0592fb367b68fda409fca327db252f9184071fb
pink <eduardoxfurtado@gmail.com>
2019-07-27 17:57:48 -0300

    Interview Preparation Kit - Warm-up Challenges - Counting Valleys

---
+++ Interview Preparation Kit/Warm-up Challenges/Counting Valleys.py
@@ -0,0 +1,45 @@
+'''https://www.hackerrank.com/challenges/counting-valleys/problem?h_l=interview&playlist_slugs%5B%5D%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D%5B%5D=warmup'''
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the countingValleys function below.
+
+def countingValleys(n, s):
+    level = 0
+    is_in_valley = False
+    valleys_visited = 0
+
+    for char in s:
+        if char.upper() == 'D':
+            level -= 1
+            if not is_in_valley and level == -1:
+                is_in_valley = True
+                valleys_visited += 1
+        else:
+            level += 1
+            if is_in_valley and level == 0:
+                is_in_valley = False
+    
+    return valleys_visited
+        
+        
+            
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    s = input()
+
+    result = countingValleys(n, s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
