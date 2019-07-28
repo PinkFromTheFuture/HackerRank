2d000f72538175e8c3618c28980de01be9088307
pink <eduardoxfurtado@gmail.com>
2019-07-28 11:28:00 -0300

    Problem Solving - Greedy

---
+++ Problem Solving/Greedy/Mark and Toys.py
@@ -0,0 +1,37 @@
+'''https://www.hackerrank.com/challenges/mark-and-toys/problem
+'''
+
+#!/bin/python3
+
+import os
+
+# Complete the maximumToys function below.
+def maximumToys(prices, k):
+    prices.sort()
+    quantity_of_toys_to_purchase = 0
+    for price in prices:
+        difference = k - price
+        if difference > 0:
+            quantity_of_toys_to_purchase += 1
+            k = difference
+        else:
+            # break
+            return quantity_of_toys_to_purchase
+    return quantity_of_toys_to_purchase
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    prices = list(map(int, input().rstrip().split()))
+
+    result = maximumToys(prices, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
