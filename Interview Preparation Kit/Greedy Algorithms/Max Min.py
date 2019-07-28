fa82b563cfb1e0c95046212bfb547de9e4e3598d
pink <eduardoxfurtado@gmail.com>
2019-07-27 21:43:37 -0300

    Interview Preparation Kit - Greedy Algorithms - Max Min

---
+++ Interview Preparation Kit/Greedy Algorithms/Max Min.py
@@ -0,0 +1,45 @@
+'''https://www.hackerrank.com/challenges/angry-children/problem?h_l=interview&playlist_slugs%5B%5D%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D%5B%5D=greedy-algorithms'''
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the maxMin function below.
+def maxMin(k, arr):
+    # start the result with a high value, max int value
+    minimum_unfairness = 0xFFFFFFFF
+
+    arr.sort(reverse=True)
+    # k is the number of elements. we adjust it to the distance
+    # from the first to the last, to avoid during math more than once
+    distance = k-1
+
+    for i in range(len(arr)-distance):
+        unfairness = arr[i] - arr[i+distance]
+        if unfairness < minimum_unfairness:
+            minimum_unfairness = unfairness
+
+    return minimum_unfairness
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    k = int(input())
+
+    arr = []
+
+    for _ in range(n):
+        arr_item = int(input())
+        arr.append(arr_item)
+
+    result = maxMin(k, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
