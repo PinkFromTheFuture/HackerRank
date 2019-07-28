390c0553b286fa91dffcb9abb152766ffc5833b3
pink <eduardoxfurtado@gmail.com>
2019-07-28 11:28:08 -0300

    Problem Solving - Bit Manipulation

---
+++ Problem Solving/Bit Manipulation/Flipping bits.py
@@ -0,0 +1,36 @@
+'''https://www.hackerrank.com/challenges/flipping-bits/problem
+'''
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the flippingBits function below.
+def flippingBits(n):
+    # in python ~  is the Binary Ones Complement. It is unary
+    # and has the effect of 'flipping' bits.
+    # to return the result as an unsigned integer, it's
+    # necessary to add the complement, 2**32 or (1 << 32)
+    # return ~n + 2**32
+    
+    # another way to do it would be to get the AND of the bits
+    # with the maximun unsigned integer. this might be actually faster
+    return ~n & 0xffffffff
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        n = int(input())
+
+        result = flippingBits(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
