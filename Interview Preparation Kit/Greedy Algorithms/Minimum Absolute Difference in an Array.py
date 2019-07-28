6a5bfca97c7cb4909265dc01252e4da95de639dc
pink <eduardoxfurtado@gmail.com>
2019-07-27 21:47:40 -0300

    Prep Kit - Greedy Algorithms - Minimum Absolute Difference in an Array

---
+++ Interview Preparation Kit/Greedy Algorithms/Minimum Absolute Difference in an Array.py
@@ -0,0 +1,43 @@
+'''https://www.hackerrank.com/challenges/minimum-absolute-difference-in-an-array/problem?h_l=interview&playlist_slugs%5B%5D%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D%5B%5D=greedy-algorithms'''
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the minimumAbsoluteDifference function below.
+def minimumAbsoluteDifference(arr):
+    # get the size of the array in a variable to call len only once
+    size_of_arr = len(arr)
+    # since we are talking about the minimum difference, we can
+    # sort the array and then only compare adjacent elements
+    arr.sort()
+
+    # initialize the minimum difference with a high value
+    minimum = arr[-1] - arr[0]
+
+    # loop through the sorted array and compared ajacent elements
+    for i in range(0, size_of_arr-1):
+        difference = abs(arr[i] - arr[i+1])
+        # if the difference is smaller than the minimum, update it
+        if difference < minimum:
+            minimum = difference
+
+    return minimum
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = minimumAbsoluteDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
