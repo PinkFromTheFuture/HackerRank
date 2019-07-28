482f8b53f216ebf33e8a9aacf0050293a6d03d19
pink <eduardoxfurtado@gmail.com>
2019-07-28 11:08:45 -0300

    Python - Math

---
+++ Python/Math/Power - Mod Power.py
@@ -0,0 +1,11 @@
+# https://www.hackerrank.com/challenges/python-power-mod-power/problem
+
+a = int(input())
+b = int(input())
+m = int(input())
+
+# Print two lines.
+# The first line should print the result of pow(a,b).
+print(pow(a,b))
+# The second line should print the result of pow(a,b,m)
+print(pow(a,b,m))
