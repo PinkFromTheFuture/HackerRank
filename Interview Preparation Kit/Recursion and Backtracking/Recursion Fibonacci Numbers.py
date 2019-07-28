1d13ce955e0482cb47abb6b31fc794d08a9722b6
pink <eduardoxfurtado@gmail.com>
2019-07-27 21:37:11 -0300

    Prep Kit - Recursion and Backtracking - Recursion: Fibonacci Numbers

---
+++ Interview Preparation Kit/Recursion and Backtracking/Recursion Fibonacci Numbers.py
@@ -0,0 +1,9 @@
+'''https://www.hackerrank.com/challenges/ctci-fibonacci-numbers/problem?h_l=interview&playlist_slugs%5B%5D%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D%5B%5D=recursion-backtracking'''
+def fibonacci(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fibonacci(n-1) + fibonacci(n-2)
+n = int(input())
+print(fibonacci(n))
